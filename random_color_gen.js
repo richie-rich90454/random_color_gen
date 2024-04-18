c0852042d6e6562e36f38341de25f2dcc28296ad
richie-rich90454 <166470680+richie-rich90454@users.noreply.github.com>
2024-04-18 11:54:35 +0800

    115418

--- random_color_gen.js
+++ random_color_gen.js
@@ -1,7 +1,7 @@
 let genHexBtn=document.getElementById("generateHexColor");
 let colorPreview=document.getElementById("color-preview");
 genHexBtn.addEventListener("click",function(){
-    let randomColor=`#${Math.floor(Math.random()*16777215).toString(16)}`;
+    let randomColor=`#${Math.floor(Math.random()*16777215).toString(16).toUpperCase()}`;
     colorPreview.style.backgroundColor=randomColor;
     colorPreview.textContent=randomColor;
 })

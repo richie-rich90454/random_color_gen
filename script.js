ab472861bbfdbf8b493cc1b669c9edfe53ce6bcf
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-05-14 15:17:48 +0800

    rewrote with jQuery to improve styling

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-let genHexBtn=document.getElementById("generateHexColor");
-let genRGBABtn=document.getElementById("generateRGBAColor");
-let genHSLABtn=document.getElementById("generateHSLAColor");
-let colorPreview=document.getElementById("color-preview");
-let colorSelect=document.getElementById("color-select");
-colorSelect.addEventListener("change",function(){
+let genHexBtn=$("#generateHexColor");
+let genRGBABtn=$("#generateRGBAColor");
+let genHSLABtn=$("#generateHSLAColor");
+let colorPreview=$("#color-preview");
+let colorSelect=$("#color-select");
+colorSelect.change(function(){
     document.getElementById("preview-group").style.display="block";
-    let colorValue=colorSelect.options[colorSelect.selectedIndex].value;
+    let colorValue=colorSelect.val();
+    $("input[type='button']").hide();
     if (colorValue=="hex"){
-        genHexBtn.style.display="block";
-        genRGBABtn.style.display="none";
-        genHSLABtn.style.display="none";
-        colorPreview.textContent="";
-        colorPreview.style.backgroundColor="#FFF";
+        genHexBtn.fadeIn();
     }
     else if (colorValue=="rgba"){
-        genHexBtn.style.display="none";
-        genRGBABtn.style.display="block";
-        genHSLABtn.style.display="none";
-        colorPreview.textContent="";
-        colorPreview.style.backgroundColor="#FFF";
+        genRGBABtn.fadeIn();
     }
-    else{
-        genHexBtn.style.display="none";
-        genRGBABtn.style.display="none";
-        genHSLABtn.style.display="block";
-        colorPreview.textContent="";
-        colorPreview.style.backgroundColor="#FFF";
+    else if (colorValue=="hsla"){
+        genHSLABtn.fadeIn();
     }
+    colorPreview.css("backgroundColor", "#FFF").text("");
 })
-genHexBtn.addEventListener("click",function(){
+genHexBtn.click(function(){
     let randomColor=`#${Math.floor(Math.random()*16777215).toString(16)}`;
     randomColor=randomColor.toUpperCase();
-    colorPreview.style.backgroundColor=randomColor;
-    colorPreview.textContent=randomColor;
+    colorPreview.fadeOut(function(){
+        colorPreview.css("backgroundColor", randomColor).text(randomColor).fadeIn();
+    });
 })
-genRGBABtn.addEventListener("click",function(){
+genRGBABtn.click(function(){
     let randomColor=`rgba(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${(Math.random()).toFixed(2)})`;
-    colorPreview.style.backgroundColor=randomColor;
-    colorPreview.textContent=randomColor;
+    colorPreview.fadeOut(function(){
+        colorPreview.css("backgroundColor", randomColor).text(randomColor).fadeIn();
+    });
 });
-genHSLABtn.addEventListener("click",function(){
+genHSLABtn.click(function(){
     let randomColor=`hsla(${Math.floor(Math.random()*360)}, ${Math.floor(Math.random()*101)}%, ${Math.floor(Math.random()*101)}%, ${(Math.random()).toFixed(2)})`;
-    colorPreview.style.backgroundColor=randomColor;
-    colorPreview.textContent=randomColor;
+    colorPreview.fadeOut(function(){
+        colorPreview.css("backgroundColor", randomColor).text(randomColor).fadeIn();
+    });
 });
